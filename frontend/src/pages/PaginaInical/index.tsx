8a6e107f851b365cb3440b81f2b27ce44ae56b19
LGugs <lgpa@icomp.ufam.edu.br>
2025-06-03 17:07:53 -0400

    added month description

--- frontend/src/pages/PaginaInical/index.tsx
+++ frontend/src/pages/PaginaInical/index.tsx
@@ -2,12 +2,12 @@ import { useEffect, useState } from "react";
 import { getMaoDeObra } from "../../services/maodeobra.service";
 import type { MaoDeObra } from "../../interfaces/GetMaoDeObra";
 import type { MesPassado } from "../../interfaces/MesPassado";
-import getAnoMes from "../../utils/getAnoMes";
+import { getAnoMes, convertMesToDesc } from "../../utils/getAnoMes";
 //import { useParams } from 'react-router-dom';
 
 export default function PaginaInicial() {
   const [maoDeObraData, setMaoDeObraData] = useState<MaoDeObra[]>();
-  const [testeValor, setTesteValor] = useState<number>(0);
+  //const [testeValor, setTesteValor] = useState<number>(0);
   const [mesPassado] = useState<MesPassado>(getAnoMes()); // sempre teremos valor do mes passado
 
   useEffect(() => {
@@ -27,7 +27,7 @@ export default function PaginaInicial() {
     <>
       <div>
         <h1>
-          {mesPassado.mes}/{mesPassado.ano}
+          {convertMesToDesc(mesPassado.mes)}/{mesPassado.ano}
         </h1>
         {maoDeObraData?.map((item: MaoDeObra, index: number) => (
           <div key={index}>
--- frontend/src/utils/getAnoMes.ts
+++ frontend/src/utils/getAnoMes.ts
@@ -1,7 +1,7 @@
 import type { MesPassado } from "../interfaces/MesPassado";
 
 // retorna o mes passado, incluindo se a data atual for do mes de Janeiro. Ele retornará Dezembro/Ano Atual -1
-export default function getAnoMes(): MesPassado {
+export function getAnoMes(): MesPassado {
   const dataAtual = new Date();
   const mesPassado = new Date(
     dataAtual.getFullYear(),
@@ -12,4 +12,29 @@ export default function getAnoMes(): MesPassado {
   const mes = (mesPassado.getMonth() + 1).toString();
 
   return { ano, mes };
+}
+
+export function convertMesToDesc(mes: string): string {
+  const meses = [
+    "Janeiro",
+    "Fevereiro",
+    "Março",
+    "Abril",
+    "Maio",
+    "Junho",
+    "Julho",
+    "Agosto",
+    "Setembro",
+    "Outubro",
+    "Novembro",
+    "Dezembro",
+  ];
+
+  const mesNumero = parseInt(mes, 10);
+
+  if (mesNumero >= 1 && mesNumero <= 12) {
+    return meses[mesNumero - 1]; // Subtrai 1 porque o índice do array começa em 0
+  } else {
+    throw new Error("Número do mês inválido. Deve ser entre 1 e 12.");
+  }
 }
